dca6bb241e8b30bdae3c9588e6c60674174a242c
Lwin Min Thant <lwin.m67@rsu.ac.th>
2026-03-29 20:42:06 +0700

    commit

--- routes/movies.js
+++ routes/movies.js
@@ -18,4 +18,36 @@ router.post("/:id/like", async (req, res) => {
   res.json({ message: "liked" });
 });
 
+/* GET comments */
+router.get("/:id/comments", async (req, res) => {
+  const [rows] = await db.query(
+    "SELECT * FROM comments WHERE movie_id = ? ORDER BY created_at DESC",
+    [req.params.id]
+  );
+
+  res.json(rows);
+});
+
+/* ADD comment */
+router.post("/:id/comments", async (req, res) => {
+  const { comment, rating } = req.body;
+
+  const [result] = await db.query(
+    "INSERT INTO comments (movie_id, comment, rating) VALUES (?, ?, ?)",
+    [req.params.id, comment, rating]
+
+    `UPDATE movies 
+   SET rating = (
+     SELECT AVG(rating) FROM comments WHERE movie_id = ?
+   )
+   WHERE id = ?`,
+  [req.params.id, req.params.id]
+  );
+
+  res.status(201).json({
+    message: "Comment added",
+    id: result.insertId,
+  });
+});
+
 module.exports = router;
