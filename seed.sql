9e442d395eb94ccf9c40f1bc6868af14b447c0ad
Lwin Min Thant <lwin.m67@rsu.ac.th>
2026-03-29 20:26:41 +0700

    commit

--- seed.sql
+++ seed.sql
@@ -6,7 +6,7 @@ VALUES
 ("Interstellar", "Space exploration", "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg", 8.6, 120),
 ("The Dark Knight", "Batman vs Joker", "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg", 9.0, 200),
 ("Avengers Endgame", "Final battle with Thanos", "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg", 8.4, 180),
-("Spider-Man No Way Home", "Multiverse chaos", "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", 8.3, 160),
+("Spider-Man No Way Home", "Multiverse chaos", "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", 8.3, 160),
 ("Joker", "Origin story of Joker", "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", 8.5, 140),
-("Frozen", "Disney animation", "https://image.tmdb.org/t/p/w500/4Mmd1Hc7gq0i2bR6F9c4cF8xZ4f.jpg", 7.5, 90),
+("Frozen", "Disney animation", "https://upload.wikimedia.org/wikipedia/en/0/05/Frozen_%282013_film%29_poster.jpg", 7.5, 90),
 ("Titanic", "Romantic tragedy", "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", 7.9, 110);
