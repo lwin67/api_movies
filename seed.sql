36e0e3f3e30b7d5e1477f3f92105230438fcaa01
Lwin Min Thant <lwin.m67@rsu.ac.th>
2026-03-29 17:31:51 +0700

    second commit

--- seed.sql
+++ seed.sql
@@ -2,11 +2,11 @@ USE movie_db;
 
 INSERT INTO movies (title, description, poster_url, rating, likes)
 VALUES
-("Inception", "Dream within a dream", "https://image.tmdb.org/t/p/w500/inception.jpg", 8.8, 100),
-("Interstellar", "Space exploration", "https://image.tmdb.org/t/p/w500/interstellar.jpg", 8.6, 120),
-("The Dark Knight", "Batman vs Joker", "https://image.tmdb.org/t/p/w500/darkknight.jpg", 9.0, 200),
-("Avengers: Endgame", "Final battle with Thanos", "https://image.tmdb.org/t/p/w500/endgame.jpg", 8.4, 180),
-("Spider-Man: No Way Home", "Multiverse chaos", "https://image.tmdb.org/t/p/w500/spiderman.jpg", 8.3, 160),
-("Joker", "Origin story of Joker", "https://image.tmdb.org/t/p/w500/joker.jpg", 8.5, 140),
-("Frozen", "Disney animated adventure", "https://image.tmdb.org/t/p/w500/frozen.jpg", 7.5, 90),
-("Titanic", "Romantic tragedy", "https://image.tmdb.org/t/p/w500/titanic.jpg", 7.9, 110);
+("Inception", "Dream within a dream", "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg", 8.8, 100),
+("Interstellar", "Space exploration", "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg", 8.6, 120),
+("The Dark Knight", "Batman vs Joker", "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg", 9.0, 200),
+("Avengers Endgame", "Final battle with Thanos", "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg", 8.4, 180),
+("Spider-Man No Way Home", "Multiverse chaos", "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", 8.3, 160),
+("Joker", "Origin story of Joker", "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", 8.5, 140),
+("Frozen", "Disney animation", "https://image.tmdb.org/t/p/w500/4Mmd1Hc7gq0i2bR6F9c4cF8xZ4f.jpg", 7.5, 90),
+("Titanic", "Romantic tragedy", "hhttps://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", 7.9, 110);
